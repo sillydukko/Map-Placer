c79145a3ff37dcf57b0c5a1b1bb42f31485378de
sillydukko <silly@dukko.org>
2026-03-19 20:36:18 +1300

    Add files via upload

--- src/main/java/com/gengilys/addon/MapAutoPlaceAddon.java
+++ src/main/java/com/gengilys/addon/MapAutoPlaceAddon.java
@@ -6,21 +6,12 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 
 public class MapAutoPlaceAddon extends MeteorAddon {
-
-    public static final Category CATEGORY = new Category("Map Art");
+    public static final Category CATEGORY = new Category("MapAutoPlace");
 
     @Override
     public void onInitialize() {
         Modules.get().add(new AutoMapPlace());
-        Modules.get().add(new MapPlacer());
-        Modules.get().add(new MapAura());
-        Modules.get().add(new AutoDick());
         Modules.get().add(new MapAdBot());
         Modules.get().add(new MapAdSpam());
-    }
-
-    @Override
-    public String getPackage() {
-        return "com.gengilys.addon";
     }
 }
